03332fbd6b1314817aa476396ebc6030283f98b9
suvmaha <suv.maha@gmail.com>
2026-06-02 19:38:38 -0700

    Ongoing updates

--- tutorials/job-hello-world/submit.sh
+++ tutorials/job-hello-world/submit.sh
@@ -26,6 +26,7 @@ anyscale job submit \
     --cloud "${CLOUD}" \
     --working-dir "${EXAMPLES_REPO}" \
     --name "job-hello-world" \
+    --env-vars "EXAMPLE_ENV_VAR=hello-from-eks" \
     -- python job_hello_world/main.py
 
 echo ""
